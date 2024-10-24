20fd0b7253f75acba93a8ddca170c7d49456a0e8
jesusalbujas <jesusramirez35000@gmail.com>
2024-10-24 15:41:07 -0400

    feat: New topics are added to command parameters

--- src/commands/slashCommands.js
+++ src/commands/slashCommands.js
@@ -6,23 +6,41 @@ require('dotenv').config();
 const commands = [
   {
     name: 'cerrar_hilo',
-    description: 'Cierra un hilo especificando un motivo y un solicitante.',
+    description: 'Cierra un hilo especificando características.',
     options: [
       {
         type: 6, // Tipo de opción: Usuario
         name: 'solicitante',
-        description: 'El usuario que solicita cerrar el hilo.',
+        description: 'El usuario que solicita crear el hilo.',
         required: true,
       },
       {
         type: 3, // Tipo de opción: String
-        name: 'motivo',
-        description: 'El motivo para cerrar el hilo.',
+        name: 'topico',
+        description: 'El topico del hilo.',
         required: true,
         choices: [
           { name: 'Ventas', value: 'ventas' },
-          { name: 'Compras', value: 'compras' },
+          { name: 'Cuentas por Pagar Nacional', value: 'cuentas_por_pagar_nacional' },
           { name: 'Órdenes de venta', value: 'ordenes_venta' },
+          { name: 'Cobranza', value: 'cobranza'},
+          { name: 'Finanzas', value: 'finanzas'},
+          { name: 'Gerencia', value: 'gerencia'},
+          { name: 'Contabilidad', value: 'contabilidad'},
+          { name: 'General', value: 'general'},
+          { name: 'Cuentas por pagar Importación', value: 'cuentas_por_pagar_importacion'},
+          { name: 'Almacén e Inventario', value: 'almacen_inventario'},
+          { name: 'Compra Nacional', value: 'compra_nacional'},
+          { name: 'Compra Importación', value: 'compra_importacion'},
+          { name: 'Tributos', value: 'tributos'},
+          { name: 'Cuentas por Cobrar', value: 'cuentas_por_cobrar'},
+          { name: 'Facturación', value: 'facturacion'},
+          { name: 'Tesoreria', value: 'tesoreria'},
+          { name: 'Producción', value: 'produccion'},
+          { name: 'Costos', value: 'costos'},
+          { name: 'Fuerza Móvil', value: 'fuerza_movil'},
+          { name: 'Control de Calidad', value: 'control_de_calidad'},
+          { name: 'Informática', value: 'informatica'}
         ],
       },
     ],
